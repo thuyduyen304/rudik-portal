2f0da10377dcf3e26bb6d3b7eb1423f56768dd06
Duyen Truong <thuyduyen304@gmail.com>
2019-04-16 10:04:52 +0200

    select + export

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3,9 +3,6 @@ $(document).ready(
         $('#search-form #knowledgeBase')
             .change(
                 function() {
-                    console.log("test" +
-                        $("#knowledgeBase").val());
-
                     $
                         .ajax({
                             type: "POST",
@@ -72,32 +69,11 @@ function search_rules_submit() {
         cache: false,
         timeout: 600000,
         success: function(data) {
+        	$('#appMsg').html('');
             $('#resultsBlock').show();
             $("#btn-search").prop("disabled", false);
             
-            var html = '';
-            var rules = new Object();
-
-            $.each(data, function(k, v) {
-                html += '<section class="popup-rule" id="popup-rule-' +
-                    v.ruleId + '" >' +
-                    '<pre>' + JSON.stringify(v, null, 4) + '</pre>' +
-                    '</section>';
-                rules[v.ruleId] = v;
-
-                if (v.ruleType == true) {
-                    data[k].content = trim_prefix(v.premise) + ' \u21D2 ' +
-                        trim_prefix(v.conclusion);
-                    data[k].type = '+';
-                } else {
-                    data[k].content = trim_prefix(v.premise) + ' \u0026	' +
-                        trim_prefix(v.conclusion) + ' \u21D2 ' +
-                        ' \u22A5 ';
-                    data[k].type = '-';
-                }
-            });
-
-            $('#results-table').DataTable({
+            var table = $('#results-table').DataTable({
             	destroy:true,
                 data: data,
                 "columns": [{
@@ -106,12 +82,31 @@ function search_rules_submit() {
                     className: 'select-checkbox',
                     orderable: false
                 }, {
-                    "data": "type",
-                    className: "dt-center"
-                }, {
-                    "data": "content",
+                    "data": "ruleType",
+                    className: "dt-center",
                     "render": function(data, type, row, meta) {
                         if (type === 'display') {
+                        	if(data == true) {
+                        		data = '+';
+                        	} else {
+                        		data = '-';
+                        	}
+                        }
+
+                        return data;
+                    }
+                }, {
+                    "data": "premise",
+                    "render": function(data, type, row, meta) {
+                        if (type === 'display') {
+                        	if (row.ruleType == true) {
+                                data = trim_prefix(row.premise) + ' \u21D2 ' +
+                                    trim_prefix(row.conclusion);
+                            } else {
+                                data = trim_prefix(row.premise) + ' \u0026	' +
+                                    trim_prefix(row.conclusion) + ' \u21D2 ' +
+                                    ' \u22A5 ';
+                            }
                             data = '<a class="popup-opener" data-rule-id="' + row.ruleId + '" href="#">' + data + '</a>';
                         }
 
@@ -148,8 +143,52 @@ function search_rules_submit() {
                     style: 'multi+shift',
                     selector: 'td:first-child'
                 },
+                dom: 'Bfrtip',
+                buttons:  [
+                	'selectAll',
+                    'selectNone',
+                    {
+                        text: 'JSON Export',
+                        action: function () {
+                        	if(table.rows( { selected: true } ).count() == 0) {
+                        		$('#appMsg').html('<div class="alert alert-info">There is no selected rows.</div>');
+                        	} else {
+                        		var export_data = [];
+                            	table.rows( { selected: true } ).every( function () {
+                            		export_data.push(this.data());
+                            	} );
+                                $.fn.dataTable.fileSave(
+                                    new Blob( [ JSON.stringify( export_data, null, 4 ) ] ),
+                                    'selected_export.json'
+                                );
+                        	}
+                        }
+                    },
+                    
+                ],
                 "pagingType": "simple_numbers",
                 "pageLength": 25,
+            });
+            
+            var html = '';
+            var rules = new Object();
+            $.each(data, function(k, v) {
+                html += '<section class="popup-rule" id="popup-rule-' +
+                    v.ruleId + '" >' +
+                    '<pre>' + JSON.stringify(v, null, 4) + '</pre>' +
+                    '</section>';
+                rules[v.ruleId] = v;
+
+//                if (v.ruleType == true) {
+//                    data[k].content = trim_prefix(v.premise) + ' \u21D2 ' +
+//                        trim_prefix(v.conclusion);
+//                    data[k].type = '+';
+//                } else {
+//                    data[k].content = trim_prefix(v.premise) + ' \u0026	' +
+//                        trim_prefix(v.conclusion) + ' \u21D2 ' +
+//                        ' \u22A5 ';
+//                    data[k].type = '-';
+//                }
             });
 
             $('#rule-details').html(html);
